e7b630c0e393a62e15b35766d4a142a1d696d131
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-04-20 21:01:57 +0530

    Time: 168 ms (55.52%), Space: 55.4 MB (67.13%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.js
@@ -0,0 +1,19 @@
+function BSTIterator(root) {
+  var stack = [];
+  return {hasNext, next};
+
+  function hasNext() {
+    return root || stack.length;
+  }
+
+  function next() {
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    var result = root.val;
+    root = root.right;
+    return result;
+  }
+}
