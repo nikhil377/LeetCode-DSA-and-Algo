5c7a792d9b7664b62938c86b81713ee16dd5acba
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-06-05 15:38:18 +0530

    Time: 83 ms (82.99%), Space: 44.8 MB (44.15%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -2,14 +2,30 @@
  * @param {number[][]} matrix
  * @return {number[][]}
  */
+// var transpose = function(matrix) {
+//     const res=[];
+//     for(let r=0;r<matrix[0].length;r++){
+//         const arr=[]
+//         for(let c=0;c<matrix.length;c++){
+//             arr.push(matrix[c][r])
+//         }
+//         res.push(arr)
+//     }
+//     return res
+// };
+
 var transpose = function(matrix) {
-    const res=[];
-    for(let r=0;r<matrix[0].length;r++){
-        const arr=[]
-        for(let c=0;c<matrix.length;c++){
-            arr.push(matrix[c][r])
+    const result = [];
+    
+    for (let i = 0; i < matrix.length; i += 1) {
+        for (let j = 0; j < matrix[0].length; j += 1) {
+            if (!result[j]) {
+                result[j] = [];
+            }
+            
+            result[j][i] = matrix[i][j];
         }
-        res.push(arr)
     }
-    return res
+    
+    return result;
 };
