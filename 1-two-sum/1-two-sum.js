88c64638f826ddc977a2f0c0b47b43331447fdaf
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-06-07 22:37:23 +0530

    Time: 78 ms (82.33%), Space: 43 MB (35.31%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -1,20 +1,30 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
+// SC: O(1) TC=O(n2)
+// var twoSum = function(nums, target) {
+//     let targetAfterOneMinus=0;
+    
+//     for(let i=0;i<nums.length;i++){
+//         targetAfterOneMinus= target-nums[i];
+//         for(let j=i+1;j<nums.length;j++){
+//             if(nums[j]==targetAfterOneMinus){
+//                 return [j, i]
+//             }
+//         }
+        
+        
+//     }
+//     return [];
+// };
+
 var twoSum = function(nums, target) {
-    let targetAfterOneMinus=0;
+   let numsMap = new Map();
     
     for(let i=0;i<nums.length;i++){
-        targetAfterOneMinus= target-nums[i];
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[j]==targetAfterOneMinus){
-                return [j, i]
-            }
+        if(numsMap.has(target-nums[i])){
+            return [numsMap.get(target-nums[i]), i]
+        }else{
+            numsMap.set(nums[i],i);
         }
-        
-        
     }
+    
     return [];
 };
