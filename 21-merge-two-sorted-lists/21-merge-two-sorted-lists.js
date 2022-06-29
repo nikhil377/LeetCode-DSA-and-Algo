b1130e8419299f5ff86d100246737d8123ffaf44
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-06-29 18:02:43 +0530

    Time: 119 ms (21.69%), Space: 44.7 MB (11.53%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -17,25 +17,49 @@
 //     return tempNode.next;
 // };
 
+// 1st solution
 
-var mergeTwoLists = function(l1, l2) {
-    let tempNode = new ListNode(0, null);
-    let tempNodeCopy= tempNode;
+// SC:O(n)+O(n)= O(n)
+// TC: O(n)
+
+// var mergeTwoLists = function(l1, l2) {
+//     let tempNode = new ListNode(0, null);
+//     let tempNodeCopy= tempNode;
    
     
-    while(l1 && l2){
+//     while(l1 && l2){
         
-     if(l1.val>l2.val){
-         tempNodeCopy.next=l2;
-         l2= l2.next;
+//      if(l1.val>l2.val){
+//          tempNodeCopy.next=l2;
+//          l2= l2.next;
       
-     }else {
-         tempNodeCopy.next=l1;
-         l1= l1.next;
-     } 
-           tempNodeCopy=tempNodeCopy.next;
+//      }else {
+//          tempNodeCopy.next=l1;
+//          l1= l1.next;
+//      } 
+//            tempNodeCopy=tempNodeCopy.next;
         
+//     }
+//     tempNodeCopy.next= l1 ||l2;
+//     return tempNode.next;
+// };
+
+
+// SC:O(1)
+// TC: O(n)
+
+// 2nd solution
+var mergeTwoLists = function(l1, l2) {
+    if(!l1 || !l2){
+        return l1? l1: l2;
     }
-    tempNodeCopy.next= l1 ||l2;
-    return tempNode.next;
+    if(l1.val<l2.val){
+     l1.next = mergeTwoLists(l1.next, l2);   
+        return l1;
+    }else{
+         l2.next = mergeTwoLists(l1, l2.next);   
+        return l2;
+    }
+
+    
 };
