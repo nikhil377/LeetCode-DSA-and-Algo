07dd137ac11c0ca97abdd63cd189f9e5f26783e5
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-12 04:11:14 +0530

    Time: 68 ms (96.11%), Space: 43.6 MB (69.59%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -6,3 +6,20 @@ var majorityElement = function(nums) {
     //return frequencyObj
     return parseInt(Object.entries(frequencyObj).sort((a,b)=>b[1]-a[1])[0][0]);
 };
+
+var majorityElement = function(nums) {
+let majorityAmount = Math.floor(nums.length / 2);
+let map = {};
+
+for(let i = 0; i < nums.length; i++) {
+    if(map[nums[i]] !== undefined) {
+        map[nums[i]]++;
+    } else {
+        map[nums[i]] = 1;
+    }
+    if(map[nums[i]] > majorityAmount) {
+        return nums[i];
+    }
+}
+return -1;
+}
