e4efe332a19162d6d6d6ee77e55de19ee1dce7df
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-15 00:48:33 +0530

    Time: 177 ms (7.52%), Space: 51.8 MB (38.26%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -1,9 +1,24 @@
+// var maxProfit = function(prices) {
+//     var min = Number.MAX_SAFE_INTEGER; 
+//     var max = 0;
+//     for (var i = 0; i < prices.length; i++) {
+//         min = Math.min(min, prices[i]);
+//         max = Math.max(max, prices[i] - min);
+//     }
+//     return max;
+// };
+
 var maxProfit = function(prices) {
-    var min = Number.MAX_SAFE_INTEGER; 
-    var max = 0;
-    for (var i = 0; i < prices.length; i++) {
-        min = Math.min(min, prices[i]);
-        max = Math.max(max, prices[i] - min);
+  let min= prices[0], max=0;
+    
+    for(let i=0;i<prices.length;i++){
+        if(max< prices[i]-min){
+            max= prices[i]-min
+        }
+        if(prices[i]<min){
+            min=prices[i]
+        }
     }
     return max;
+    
 };
