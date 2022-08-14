df7e92c69a93a47ff20a3a7d850791ab0ae88a55
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-14 22:32:32 +0530

    Time: 60 ms (96.43%), Space: 42 MB (79.38%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    if(nums.indexOf(target)!=-1) return (nums.indexOf(target))
+return -1;
+     
+
+}
