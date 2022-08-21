5ae85c1a9e15e08754ca1f08564b7d37e8ac179c
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-21 21:58:17 +0530

    Time: 117 ms (60.46%), Space: 43.1 MB (79.77%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -1,34 +1,36 @@
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
-var isAnagram = function(s, t) {
-    // frequency pattern solution
-    s= s.split("").sort((a,b)=>a > b ? 1: -1).join("");
-    t= t.split("").sort((a,b)=>a > b ? 1: -1).join("");
+
+// var isAnagram = function(s, t) {
+//     // frequency pattern solution
+//     s= s.split("").sort((a,b)=>a > b ? 1: -1).join("");
+//     t= t.split("").sort((a,b)=>a > b ? 1: -1).join("");
     
-    if(s.length!=t.length) return false;
+//     if(s.length!=t.length) return false;
     
-   return s==t;
+//    return s==t;
     
     
     
    
+// };
+
+var isAnagram = function(s, t) {
+ 
+    if(s.length!=t.length) return false;
+    
+    let firstStringFreq={};
+  
+    for(let i=0;i<s.length;i++){
+        if(!firstStringFreq[s[i]]){
+            firstStringFreq[s[i]]= 1;
+        }else{
+            ++ firstStringFreq[s[i]]
+        }
+    }
+    for(let i=0;i<t.length;i++){
+        let letter= t[i];
+        if(!firstStringFreq[letter]) return false;
+        else firstStringFreq[letter]--;
+    }
+    
+    return true;
 };
-
-
- // if(s.length!=t.length) return false;
- //    let firstStringFreq={};
- //    for(let i=0;i<s.length;i++){
- //        firstStringFreq[s[i]]? ++firstStringFreq[s[i]]:firstStringFreq[s[i]]=1;
- //    }
- //    for(let i=0;i<t.length;i++){
- //       letter= t[i];
- //        if(!firstStringFreq[letter]){
- //            return false;
- //        }else{
- //            firstStringFreq[letter]-=1;
- //        }
- //    }
- //    return true;
