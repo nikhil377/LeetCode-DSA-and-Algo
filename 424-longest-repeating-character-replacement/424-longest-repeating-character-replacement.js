8b4b0207a86686cc3e2ee6d90a1b995e1efd89ab
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-23 23:41:57 +0530

    Time: 111 ms (70.78%), Space: 42.4 MB (81.59%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -1,22 +1,43 @@
+//  Take 2 pointers
+//  Left and Right
+//  move right pointer one by one over the loop
+//  keep track of no of occurances of chars in object
+// if occurance exceeds max count update the max count
+// if length of string of window + maxcount exceeds the k that is occurance then we shrink window from left and move left pointer
+// at the end return right-left that is window size;
+
+
+
 const characterReplacement = (s, k) => {
-  let left = 0;
-  let right = 0;
-  let maxCharCount = 0;
-  const visited = {};
-
-  while (right < s.length) {
-    const char = s[right];
-    visited[char] = visited[char] ? visited[char] + 1 : 1;
-
-    if (visited[char] > maxCharCount) maxCharCount = visited[char];
-
-    if (right - left + 1 - maxCharCount > k) {
-      visited[s[left]]--;
-      left++;
+ 
+    let left=0, right=0;
+    let countOfChars={}
+    let maxCount=0;
+    
+    for(let i=0;i<s.length;i++){
+        
+        let char =s[i];
+       
+        if(countOfChars[char]){
+            countOfChars[char]++;
+        }else{
+            countOfChars[char] =1;
+        }
+        
+        if(countOfChars[char] >maxCount ){
+            maxCount= countOfChars[char] ;
+        }
+        
+        if(right-left+1 - maxCount > k){
+            countOfChars[s[left]]--;
+            left++;
+        }
+        
+        right++;
+        
     }
-
-    right++;
-  }
-
-  return right - left;
+    
+   return right-left;
+  
+    
 };
