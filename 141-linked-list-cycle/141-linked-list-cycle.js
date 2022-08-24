983101487742ecb42bced4cc0458be0063a09e22
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-24 23:00:27 +0530

    Time: 117 ms (39.24%), Space: 46.1 MB (7.88%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -10,6 +10,9 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
+// SC : O(1)
+// TC: O(n)
 var hasCycle = function(head) {
     let headCopy= head;
     let slowPointer= head, fastPointer= head;
@@ -26,4 +29,20 @@ var hasCycle = function(head) {
     
     return false;
     
+};
+
+
+// SC : O(n)
+// TC: O(n)
+
+var hasCycle = function(head) {
+    const seen = new Set();
+    
+    function traverse(node) {
+        if(seen.has(node)) return true;
+        if(!node) return false;
+        seen.add(node);
+        return traverse(node.next);
+    }
+    return traverse(head);
 };
