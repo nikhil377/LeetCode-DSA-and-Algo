d6ebb3e5bbd502b734534033c4b38c5c963d4535
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-25 03:50:22 +0530

    Time: 117 ms (28.19%), Space: 44.2 MB (61.18%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -1,22 +1,3 @@
-// var mergeTwoLists = function(l1, l2) {
-//     let tempNode = new ListNode(0, null);
-//     let currentNode = tempNode;
-    
-//     while(l1 && l2){
-//         if(l1.val<l2.val){
-//             currentNode.next=l1;
-//             l1=l1.next;
-//         }else{
-//             currentNode.next = l2;
-//             l2=l2.next;
-//         }
-//         currentNode= currentNode.next;
-//     }
-//    currentNode.next=  l1 || l2;
-   
-//     return tempNode.next;
-// };
-
 // 1st solution
 
 // SC:O(n)+O(n)= O(n)
@@ -45,21 +26,40 @@
 // };
 
 
-// SC:O(1)
+// SC:O(n+m)
 // TC: O(n)
 
-// 2nd solution
-var mergeTwoLists = function(l1, l2) {
-    if(!l1 || !l2){
-        return l1? l1: l2;
-    }
-    if(l1.val<l2.val){
-     l1.next = mergeTwoLists(l1.next, l2);   
-        return l1;
-    }else{
-         l2.next = mergeTwoLists(l1, l2.next);   
-        return l2;
-    }
+// 2nd solution recursive
+// var mergeTwoLists = function(l1, l2) {
+//     if(!l1 || !l2){
+//         return l1? l1: l2;
+//     }
+//     if(l1.val<l2.val){
+//      l1.next = mergeTwoLists(l1.next, l2);   
+//         return l1;
+//     }else{
+//          l2.next = mergeTwoLists(l1, l2.next);   
+//         return l2;
+//     }
 
     
+// };
+
+var mergeTwoLists = function(l1, l2) {
+    let tempNode = new ListNode(0, null);
+    let currentNode = tempNode;
+    
+    while(l1 && l2){
+        if(l1.val<l2.val){
+            currentNode.next=l1;
+            l1=l1.next;
+        }else{
+            currentNode.next = l2;
+            l2=l2.next;
+        }
+        currentNode= currentNode.next;
+    }
+   currentNode.next=  l1 || l2;
+   
+    return tempNode.next;
 };
