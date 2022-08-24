fa49a332beb6034641d94b7972333d0da0d86296
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-08-24 22:21:24 +0530

    Time: 130 ms (6.77%), Space: 44.8 MB (9.84%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -9,34 +9,46 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-   let current= head, prev=null;
-    
-    while(current){
-        let holdNext= current.next;
-        current.next=prev;
-        prev=current;
-        current=holdNext;
-        
-        
-    }
-    return prev;
-
-    
-};
 
 
-// // recursive call
+// SC O(1)
+// TC O(n)
 // var reverseList = function(head) {
-// 	// base case
-//     if (head == null || head.next == null){
-//         return head;
+//    let current= head, prev=null;
+    
+//     while(current){
+//         let holdNext= current.next;
+//         current.next=prev;
+//         prev=current;
+//         current=holdNext;
+        
+        
 //     }
-// 	// go all the way to the end
-//     let reversedListHead = reverseList(head.next)
-// 	// add reverse myself
-//     head.next.next = head;
-//     head.next = null;
-// 	// go up
-//     return reversedListHead
+//     return prev;
+
+    
 // };
+
+// // SC O(1)
+// // TC O(n)
+// var reverseList = function(head) {
+//     let [prev, current] = [null, head]
+//     while(current) {
+//         [current.next, prev, current] = [prev, current, current.next]
+//     }
+//     return prev
+// }
+
+
+// recursive call
+var reverseList = function(head) {
+    
+    if(head== null || head.next==null){
+        return head;
+    }
+    
+    let newHead= reverseList(head.next);
+    head.next.next=head;
+    head.next=null;
+    return newHead;
+};
