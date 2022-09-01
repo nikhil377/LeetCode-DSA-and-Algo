0568cc87e8629d45bcfeda345a1bc3183fd6de73
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-09-02 00:20:28 +0530

    Time: 277 ms (22.25%), Space: 83.3 MB (37.08%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let num="";
+    while(head){
+        num+=head.val;
+        head=head.next;
+        
+    }
+  function reverseString(str) {
+
+    // empty string
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+    
+    return (num==reverseString(num)) 
+};
