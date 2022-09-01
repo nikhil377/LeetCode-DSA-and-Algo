52f0cfb77c85370cf80535eee20ad48ceaa5b9de
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-09-02 00:49:22 +0530

    Time: 219 ms (55.15%), Space: 69.7 MB (79.69%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.js
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -1,30 +1,69 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {boolean}
- */
-var isPalindrome = function(head) {
-    let num="";
-    while(head){
-        num+=head.val;
-        head=head.next;
-        
-    }
-  function reverseString(str) {
 
-    // empty string
-    let newString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i];
-    }
-    return newString;
-}
+// Algo
+//  one approach is to take a string and add the numbers to it
+//  reverse the string and check if original and reversed is matching or not
+//  if yes return true otherwise false
+
+
+// 2nd optimized approach is-->
+// traverse till mid of linkedlist
+// find middle (use slow and fast pointer approach)
+// reverse the 2nd half of the list
+// now compare 1st half and 2nd half of list
+// at any point in while loop if they aren't equal return false else return true
+
+// var isPalindrome = function(head) {
+//    let headCopy= head;
+//     let arr=[];
     
-    return (num==reverseString(num)) 
+    
+//     while(headCopy){
+//         arr.push(headCopy.val);
+//         headCopy = headCopy.next;
+        
+//     }
+//      if(arr.join('') == arr.reverse().join('')) {
+//          return true;
+//      }
+//      else {return false}
+// };
+
+// best approach
+
+var isPalindrome = function(head) {
+    if(!head || !head.next) return true;
+    
+    var slowPointer=head;
+    var fastPointer=head;
+    
+    while(fastPointer.next && fastPointer.next.next){
+       
+        slowPointer= slowPointer.next;
+         fastPointer= fastPointer.next.next;
+    }
+    
+    var reversedList = reverseList(slowPointer.next);
+    
+    slowPointer=reversedList; 
+    
+    while(slowPointer!=null){
+        if(head.val!=slowPointer.val) return false
+        head=head.next;
+        slowPointer= slowPointer.next;
+    }
+    
+    return true;
+
+};
+
+var reverseList = function(head) {
+        var pre = null;
+        var next = null;
+        while(head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
 };
