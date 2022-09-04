e865ccde451a5a9bf770e7d2a8623c6983ad9115
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-09-04 17:52:48 +0530

    Time: 108 ms (49.04%), Space: 45.9 MB (83.78%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -7,17 +7,24 @@
  * }
  */
 
-// var isValidBST = function(root, min = -Infinity, max = Infinity) {
-//     if(root === null)
-//         return true;
-//     if(root.val <= min || root.val >= max)
-//         return false;
-//     return isValidBST(root.right, root.val, max) && isValidBST(root.left, min, root.val)
-// };
-
-var isValidBST = function(root, min=null, max=null) {
-    if (!root) return true;
-    if (min && root.val <= min.val) return false;
-    if (max && root.val >= max.val) return false;
-    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+// SC--O(n) TC--O(n) space complexity is O(n) because it's recursion 
+// 1st solution
+var isValidBST = function(root, min = -Infinity, max = Infinity) {
+    if(root==null){
+        return true;
+    }
+    if(root.val<=min || root.val>=max){
+        return false;
+    }
+    
+    return isValidBST(root.left,min,root.val) && isValidBST(root.right, root.val, max); 
 };
+
+
+// 2nd solution
+// var isValidBST = function(root, min=null, max=null) {
+//     if (!root) return true;
+//     if (min && root.val <= min.val) return false;
+//     if (max && root.val >= max.val) return false;
+//     return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+// };
