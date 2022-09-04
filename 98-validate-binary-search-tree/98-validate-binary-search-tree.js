01f21e064b5918ad4775d8c3651121b59538e170
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-09-04 17:47:03 +0530

    Time: 105 ms (53.85%), Space: 46.7 MB (27.13%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -6,14 +6,18 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
-var isValidBST = function(root, min = -Infinity, max = Infinity) {
-    if(root === null)
-        return true;
-    if(root.val <= min || root.val >= max)
-        return false;
-    return isValidBST(root.right, root.val, max) && isValidBST(root.left, min, root.val)
+
+// var isValidBST = function(root, min = -Infinity, max = Infinity) {
+//     if(root === null)
+//         return true;
+//     if(root.val <= min || root.val >= max)
+//         return false;
+//     return isValidBST(root.right, root.val, max) && isValidBST(root.left, min, root.val)
+// };
+
+var isValidBST = function(root, min=null, max=null) {
+    if (!root) return true;
+    if (min && root.val <= min.val) return false;
+    if (max && root.val >= max.val) return false;
+    return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
 };
