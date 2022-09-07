cebe47d232074db529a33b2597c746e4cb54035f
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-09-08 02:26:00 +0530

    Time: 105 ms (36.88%), Space: 43.7 MB (72.34%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.js
@@ -11,25 +11,69 @@
  * @return {TreeNode}
  */
 
-const bstFromPreorder = function (preorder) {
-  let i = 0
-  const process = (bound) => {
-    if (i === preorder.length || preorder[i] > bound) {
-      return null
-    }
+// const bstFromPreorder = function (preorder) {
+//   let i = 0
+//   const process = (bound) => {
+//     if (i === preorder.length || preorder[i] > bound) {
+//       return null
+//     }
 
-    const num = preorder[i]
-    const node = new TreeNode(num)
-    i++
+//     const num = preorder[i]
+//     const node = new TreeNode(num)
+//     i++
 
-    node.left = process(node.val)
-    node.right = process(bound)
+//     node.left = process(node.val)
+//     node.right = process(bound)
 
-    return node
+//     return node
+//   }
+
+//   const root = process(Number.MAX_SAFE_INTEGER)
+
+//   return root
+// }
+
+
+
+
+// 2nd solution
+
+const bstFromPreorder = (preorder) => {
+  // base cases
+  if (preorder.length === 0) return null;
+  if (preorder.length === 1) return new TreeNode(preorder[0]);
+
+  // result tree node
+  var res = new TreeNode(preorder[0]);
+
+  // iterate from 1 to n
+  for (let i = 1; i < preorder.length; i++) {
+    // insert this num
+    res = insert(res, preorder[i]);
   }
 
-  const root = process(Number.MAX_SAFE_INTEGER)
+  return res;
+};
 
-  return root
-}
+/**
+ * recursive func to insert value into tree
+ * @param {TreeNode} root
+ * @param {number} val val to insert
+ * @return {TreeNode} altered node
+ */
+const insert = (root, val) => {
+  // base
+  if (root === null) return new TreeNode(val);
 
+  // compare value between val and root
+  if (val < root.val) {
+    // insert in left subtree
+    root.left = insert(root.left, val);
+  } else {
+    // insert in right subtree
+    root.right = insert(root.right, val);
+  }
+
+  // return root w/ val inserted
+  return root;
+};
