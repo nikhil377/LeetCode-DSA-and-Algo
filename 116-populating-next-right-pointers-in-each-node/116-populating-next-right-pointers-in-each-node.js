7c79c131566ceb4daca48296a34ee3c9c202e92e
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-09-11 04:08:24 +0530

    Time: 123 ms (46.28%), Space: 48.5 MB (74.70%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.js
@@ -20,3 +20,20 @@ var connect = function(root) {
     connect(root.right);
     return root;
 };
+
+
+// 
+  // O(1) space,  O(n) time 
+var connect = function(root) {
+    let ptr = root;
+    while(root && root.left){
+        let temp = root;
+        while(temp) {
+            temp.left.next = temp.right;
+            temp.right.next = temp.next ? temp.next.left : null;
+            temp = temp.next;
+        }
+        root = root.left;
+    }
+    return ptr;
+}
