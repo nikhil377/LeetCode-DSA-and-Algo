e0fc4e5f5e899895a8e3ad498478e81aff26aab2
Nikhil Arora <37083944+nikhil377@users.noreply.github.com>
2022-09-12 02:30:30 +0530

    Time: 1890 ms (6.78%), Space: 49.6 MB (11.38%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -2,13 +2,24 @@
  * @param {number[]} arr
  * @return {number[]}
  */
+// const replaceElements = arr => {
+//     const result = new Array(arr.length);
+//     result[arr.length - 1] = -1;
+    
+//     for (let i = arr.length - 1; i > 0; i -= 1) {
+//         result[i - 1] = Math.max(arr[i], result[i]);
+//     }
+    
+//     return result;
+// };
+
+
 const replaceElements = arr => {
-    const result = new Array(arr.length);
-    result[arr.length - 1] = -1;
+    let len=arr.length;
     
-    for (let i = arr.length - 1; i > 0; i -= 1) {
-        result[i - 1] = Math.max(arr[i], result[i]);
-    }
+    return arr.map((data,index)=> index==len-1 ? -1 : Math.max(...arr.slice(index+1, len)));
     
-    return result;
+    
+    
+    
 };
